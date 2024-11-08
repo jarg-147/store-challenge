f9ae3ca381b56951e89de0671ddc6cb44e912547
jarg-147 <javier@jargcode.com>
2024-11-08 18:13:09 +0100

    core(network): Add implementations in gradle file

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.store.android.library)
     alias(libs.plugins.store.android.library.flavors)
     alias(libs.plugins.store.hilt)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 android {
@@ -11,5 +12,11 @@ android {
 }
 
 dependencies {
-
+    // Modules
+    implementation(projects.core.domain)
+    // Dependencies
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.okhttp)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.kotlinx.serialization.json)
 }
