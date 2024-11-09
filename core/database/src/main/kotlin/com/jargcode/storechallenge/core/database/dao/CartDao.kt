276f1a7725238ec17e2e1c05fc7373effe049483
jarg-147 <javier@jargcode.com>
2024-11-09 13:04:46 +0100

    core(database): Update cart DAO and CartItemEntity

--- core/database/src/main/kotlin/com/jargcode/storechallenge/core/database/dao/CartDao.kt
+++ core/database/src/main/kotlin/com/jargcode/storechallenge/core/database/dao/CartDao.kt
@@ -2,14 +2,21 @@ package com.jargcode.storechallenge.core.database.dao
 
 import androidx.room.*
 import com.jargcode.storechallenge.core.database.model.CartItemEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CartDao {
 
+    @Query("SELECT * FROM cart_item")
+    fun getUserCartItems(): Flow<List<CartItemEntity>>
+
+    @Query("SELECT SUM(quantity) FROM cart_item")
+    fun getCartCount(): Flow<Int>
+
     @Upsert
     suspend fun upsertProduct(entity: CartItemEntity)
 
-    @Query("SELECT * FROM cart_item WHERE product_id = :id")
+    @Query("SELECT * FROM cart_item WHERE product_code = :id")
     suspend fun getCartItem(id: String): List<CartItemEntity>
 
     suspend fun addProductUnitToCart(productId: String) {
@@ -22,5 +29,8 @@ interface CartDao {
             upsertProduct(CartItemEntity(productId, 1))
         }
     }
+
+    @Query("DELETE FROM cart_item WHERE product_code = :productCode")
+    suspend fun removeCartProduct(productCode: String)
 
 }
--- core/database/src/main/kotlin/com/jargcode/storechallenge/core/database/model/CartItemEntity.kt
+++ core/database/src/main/kotlin/com/jargcode/storechallenge/core/database/model/CartItemEntity.kt
@@ -1,14 +1,20 @@
 package com.jargcode.storechallenge.core.database.model
 
 import androidx.room.*
+import com.jargcode.storechallenge.core.domain.cart.model.SavedCartItem
 
 @Entity(
     tableName = "cart_item",
 )
 data class CartItemEntity(
     @PrimaryKey
-    @ColumnInfo(name = "product_id")
-    val productId: String,
+    @ColumnInfo(name = "product_code")
+    val productCode: String,
     @ColumnInfo(name = "quantity")
     val quantity: Int,
+)
+
+fun CartItemEntity.toSavedCartItem() = SavedCartItem(
+    productCode = productCode,
+    quantity = quantity,
 )
