d6a0357b12b1081e58ae06765cbfb54152b0bf21
jarg-147 <javier@jargcode.com>
2024-11-10 01:12:52 +0100

    core(checkout): Add model and use case

---
+++ core/domain/src/main/kotlin/com/jargcode/storechallenge/core/domain/checkout/model/CheckoutSummary.kt
@@ -0,0 +1,11 @@
+package com.jargcode.storechallenge.core.domain.checkout.model
+
+import com.jargcode.storechallenge.core.domain.cart.model.Cart.CartProduct
+import com.jargcode.storechallenge.core.domain.discounts.model.AppliedDiscount
+
+data class CheckoutSummary(
+    val checkoutItems: List<CartProduct>,
+    val subtotal: Double,
+    val appliedDiscounts: List<AppliedDiscount>,
+    val total: Double,
+)
