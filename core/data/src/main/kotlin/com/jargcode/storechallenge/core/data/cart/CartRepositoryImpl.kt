046a5054b4018b5ca99fcfba5dbf40630b4dc2da
jarg-147 <javier@jargcode.com>
2024-11-11 17:08:18 +0100

    core(data): Rename item to product for better readability

--- core/data/src/main/kotlin/com/jargcode/storechallenge/core/data/cart/CartRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/jargcode/storechallenge/core/data/cart/CartRepositoryImpl.kt
@@ -1,9 +1,9 @@
 package com.jargcode.storechallenge.core.data.cart
 
 import com.jargcode.storechallenge.core.database.dao.CartDao
-import com.jargcode.storechallenge.core.database.model.toSavedCartItem
+import com.jargcode.storechallenge.core.database.model.toSavedCartProduct
 import com.jargcode.storechallenge.core.domain.cart.CartRepository
-import com.jargcode.storechallenge.core.domain.cart.model.SavedCartItem
+import com.jargcode.storechallenge.core.domain.cart.model.SavedCartProduct
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -13,16 +13,16 @@ class CartRepositoryImpl @Inject constructor(
 ) : CartRepository {
 
     override suspend fun addProduct(productCode: String) {
-        cartDao.addProductUnitToCart(productCode = productCode)
+        cartDao.addProductToCart(productCode = productCode)
     }
 
     override fun getCartCount(): Flow<Int> = cartDao.getCartCount()
 
-    override fun getUserCartItems(): Flow<List<SavedCartItem>> = cartDao
-        .getUserCartItems()
-        .map {  cartItemEntities ->
-            cartItemEntities.map { cartItemEntity ->
-                cartItemEntity.toSavedCartItem()
+    override fun getCartProducts(): Flow<List<SavedCartProduct>> = cartDao
+        .getCartProducts()
+        .map { cartProductsEntities ->
+            cartProductsEntities.map { cartProductEntity ->
+                cartProductEntity.toSavedCartProduct()
             }
         }
 
